91a2ba46dc1487462abbe61355914a8394bb2b13
katerinavp <bl_ev@mail.ru>
2020-12-04 14:56:51 +0300

    Добавлены иконки: лайк, комментарии, поделиться.

--- app/src/main/java/com/example/a1first_application/MainActivity.kt
+++ app/src/main/java/com/example/a1first_application/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.a1first_application
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
 
-abstract class MainActivity : AppCompatActivity(), View.OnClickListener {
-
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val post = Post(12, "02 december 2020", "Petrov", "This is simple content", false)
-
         txtPost.text = post.content
         txtDate.text = post.date
 
@@ -24,21 +22,63 @@ abstract class MainActivity : AppCompatActivity(), View.OnClickListener {
         imageLike.setOnClickListener(this)
         imageComment.setOnClickListener(this)
         imageShare.setOnClickListener(this)
-
     }
+
+    var countLike: Int = 0
+    var countComment: Int = 0
+    var countShare: Int = 0
+
 
     override fun onClick(v: View) {
         val item_id = v.id
         when (item_id) {
-            R.id.imageLike ->
-                imageLike.setImageResource(R.drawable.ic_baseline_favorite_border_24_red)
+            R.id.imageLike -> {
+                if (countLike == 0) {
+                    countLike += 1
+                    imageLike.setImageResource(R.drawable.ic_baseline_favorite_border_24_red)
+                    txtCountLike.text = countLike.toString()
+                    txtCountLike.setTextColor(Color.RED)
+                } else {
+                    countLike -= 1
+                    imageLike.setImageResource(R.drawable.ic_baseline_favorite_border_24)
+                    txtCountLike.text = countLike.toString()
+                    txtCountLike.setTextColor(Color.WHITE)
+                }
+            }
+            R.id.imageComment -> {
+                if (countComment == 0) {
+                    countComment += 1
+                    imageComment.setImageResource(R.drawable.ic_baseline_chat_bubble_outline_24_red)
+                    txtCountComment.text = countComment.toString()
+                    txtCountComment.setTextColor(Color.RED)
+                } else {
+                    countComment -= 1
+                    imageComment.setImageResource(R.drawable.ic_baseline_chat_bubble_outline_24)
+                    txtCountComment.text = countComment.toString()
+                    txtCountComment.setTextColor(Color.WHITE)
+                }
+            }
+            R.id.imageShare -> {
+                if (countShare == 0) {
+                    countShare += 1
+                    imageShare.setImageResource(R.drawable.ic_baseline_share_24_red)
+                    txtCountShare.text = countShare.toString()
+                    txtCountShare.setTextColor(Color.RED)
+                } else {
+                    countShare -= 1
+                    imageShare.setImageResource(R.drawable.ic_baseline_share_24)
+                    txtCountShare.text = countShare.toString()
+                    txtCountShare.setTextColor(Color.WHITE)
+                }
+            }
 
-            R.id.imageComment ->
-                imageLike.setImageResource(R.drawable.ic_baseline_chat_bubble_outline_24_red)
 
         }
     }
 }
+
+
+
 
 
 
--- app/src/main/java/com/example/a1first_application/Post.kt
+++ app/src/main/java/com/example/a1first_application/Post.kt
@@ -1,6 +1,14 @@
 package com.example.a1first_application
 
-class Post(val id: Int, val date: String, val author: String, val content: String, var likedByMe: Boolean = false ){
+class Post(
+    val id: Int,
+    val date: String,
+    val author: String,
+    val content: String,
+    var likedByMe: Boolean = false
+) {
+
 
 }
+
 
